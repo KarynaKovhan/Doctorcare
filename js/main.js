3eb9bbf7d9c4320298adfea012ae6585dfcb8cdf
KarynaKovhan <karinakovgan3006@gmail.com>
2022-06-07 23:59:12 +0300

    added hover on button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 function burgerMenu(icon) {
     icon.classList.toggle("change");
 }
-
-elem.onclick = function() { 
-    alert('Станет');
-  };
+const getModalWindow = () => alert('Contact us via Viber : +3080505005');
+const button = document.querySelectorAll(".button-normal");
+Array.from(button).forEach(elem => elem.addEventListener("click", () => getModalWindow()));
