bd2c51e19db89523228d8d8465517374720aa59d
KarynaKovhan <karinakovgan3006@gmail.com>
2022-06-10 12:23:25 +0300

    added navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-function burgerMenu(icon) {
-    icon.classList.toggle("change");
-}
 const getModalWindow = () => alert('Contact us via Viber : +3080505005');
 const button = document.querySelectorAll(".button-normal");
 Array.from(button).forEach(elem => elem.addEventListener("click", () => getModalWindow()));
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+ 
+const menu = document.querySelector("#menu").cloneNode(1);
+ 
+hamb.addEventListener("click", hambHandler);
+ 
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+ 
+function renderPopup() {
+  popup.appendChild(menu);
+}
+ 
+const links = Array.from(menu.children);
+ 
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+ 
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
