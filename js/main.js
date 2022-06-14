5a1a3d9a2fd4e20d13618e32b03028abbd2f1f96
KarynaKovhan <karinakovgan3006@gmail.com>
2022-06-15 00:17:09 +0300

    changed color while scrolling header

--- js/main.js
+++ js/main.js
@@ -38,3 +38,39 @@ function closeOnClick() {
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+  var currentScrollPos = window.pageYOffset;
+  let header = document.getElementsByClassName("navbar")[0];
+  let button = document.getElementsByClassName("button-normal")[1];
+  let headerLogo = document.getElementsByClassName("title-a")[0];
+  let headerLogoSecond = document.getElementsByClassName("title-a-second")[0];
+  let headerText = document.getElementsByClassName("color")[0];
+  let headerTextSecond = document.getElementsByClassName("color")[1];
+  let headerTextThird = document.getElementsByClassName("color")[2];
+  let headerTextFourth = document.getElementsByClassName("color")[3];
+
+  if (prevScrollpos < currentScrollPos) {
+    header.classList.add("change-header-color");
+    button.classList.add("change-button-color");
+    headerLogo.classList.add("change-headerlogo-color");
+    headerLogoSecond.classList.add("change-headerlogo-color-second");
+    headerText.classList.add("change-headertext-color");
+    headerTextSecond.classList.add("change-headertext-color");
+    headerTextThird.classList.add("change-headertext-color");
+    headerTextFourth.classList.add("change-headertext-color");
+
+  } else {
+    header.classList.remove("change-header-color");
+    button.classList.remove("change-button-color");
+    headerLogo.classList.remove("change-headerlogo-color");
+    headerLogoSecond.classList.remove("change-headerlogo-color-second");
+    headerText.classList.remove("change-headertext-color");
+    headerTextSecond.classList.remove("change-headertext-color");
+    headerTextThird.classList.remove("change-headertext-color");
+    headerTextFourth.classList.remove("change-headertext-color");
+  }
+  prevScrollpos = currentScrollPos;
+}
